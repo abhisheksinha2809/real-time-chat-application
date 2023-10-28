4f16a9d74d65216fb9b679409427344e9b6250a1
abhisheksinha2809 <121453013+abhisheksinha2809@users.noreply.github.com>
2023-10-28 23:33:16 +0530

    latest update

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -101,8 +101,8 @@ const Signup = () => {
       const data = new FormData();
       data.append("file", pics);
       data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "dfcg2zogq");
-      fetch("https://api.cloudinary.com/v1_1/dfcg2zogq/image/upload", {
+      data.append("cloud_name", "piyushproj");
+      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
         method: "post",
         body: data,
       })
